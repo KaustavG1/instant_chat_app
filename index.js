6ede84d90a2e9ec24c895796a9ec8c75fab064f5
KaustavG1 <mail2kg2@gmail.com>
2021-02-26 16:10:22 +0530

    Requirement completed

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const { response } = require('express');
-const { request } = require('http');
-
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const mongoose = require('mongoose');
+
+// Connect to DB
+mongoose.connect("mongodb://localhost:27017/chatDB").then(() => console.log('Connection Successful'));
+
+// Serve UI to client
 app.get('/', (request, response) => {
     response.sendFile(__dirname + "/index.html");
-    // console.log("New connection");
 });
 
-// io.on('connection', (socket) => {
-//     console.log('New user connected');
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', msg);
-//     });
-// });
+// Create DB Schema
+const chatDBSchema = new mongoose.Schema({
+    message: String
+});
 
+// Create Schema Model
+const chatModel = mongoose.model('client_replies', chatDBSchema);
+
+// Initiate socket connection, broadcast 'Hello World' and listen for incomming massages; eventually save incomming messages to client_replies collection of chatDB database
 io.on('connection', (socket) => {
-    //console.log("Client connected!");
     socket.on('message-from-client-to-server', (msg) => {
-        console.log(msg);
+        const messageObj = new chatModel({
+            message: msg
+        });
+        messageObj.save().then(doc => {console.log(doc)}).catch(err => {console.log('ERROR', err)});
     })
     socket.emit('message-from-server-to-client', 'Hello World!');
 });
 
+// Listen for connections on port 8080
 http.listen(8080, () => {
     console.log('listening on port 8080');
 });
